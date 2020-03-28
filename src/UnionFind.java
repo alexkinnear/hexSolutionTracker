5b9e8e2d3e066ed5d8a72c558311f52d36723b58
Alex Kinnear <alexander.kinnear@aggiemail.usu.edu>
2020-03-28 01:55:38 -0700

    Began writing Union Find with path compression

---
+++ src/UnionFind.java
@@ -0,0 +1,57 @@
+// Union by Size with Path Compression
+public class UnionFind {
+    int size;
+    private int[] id;
+    private int[] treeSize;
+
+    UnionFind(int size) {
+        this.size = size;
+        id = new int[size];
+        treeSize = new int[size];
+
+        // Initialize the elements to be their own roots (trees of size 1)
+        for (int i = 0; i < size; i++) {
+            id[i] = i;
+            treeSize[i] = 1;
+        }
+
+    }
+
+    // Union by size
+    public void Union(int a, int b) {
+        int num1 = find(a);
+        int num2 = find(b);
+
+        if (num1 == num2) return;   // same root, nothing happens
+
+        if (treeSize[num1] > treeSize[num2]) {
+            treeSize[num1] += treeSize[num2];
+            id[num2] = id[num1];
+        }
+        else {
+            treeSize[num2] += treeSize[num1];
+            id[num1] = id[num2];
+        }
+
+
+    }
+
+    // Find element and return root with path compression along the way
+    public int find(int i) {
+        int root = i;
+        while (root != id[root]) {
+            root = id[root];
+        }
+
+        while (i != root) {
+            int num = id[i];
+            id[i] = num;
+            i = num;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
