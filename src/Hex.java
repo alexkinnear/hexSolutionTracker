abbbd4d5effa3aef8845e0b17a872a7dd83c921d
Alex Kinnear <alexander.kinnear@aggiemail.usu.edu>
2020-04-08 20:42:17 -0600

    Need to work on checking all previous moves in p1 and p2 list to Union

--- src/Hex.java
+++ src/Hex.java
@@ -1,5 +1,3 @@
-import java.awt.*;
-import java.io.Console;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -7,21 +5,21 @@ import java.util.Scanner;
 
 public class Hex {
 
-    public static final String RED = "\033[0;31m";     // RED
-    public static final String BLUE = "\033[0;34m";    // BLUE
-    public static final String BLACK = "\033[0;30m";   // BLACK
+    public static final String RED = "\u001B[31m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String RESET = "\u001B[0m";
 
-    private static void printBoard(UnionFind board, ArrayList p1, ArrayList p2) {
+    private static void printBoard(ArrayList p1, ArrayList p2) {
         int count = 1;
         StringBuilder indent = new StringBuilder();
         for (int j = 1; j <= 11; j++) {
             System.out.print(indent);
             for (int i = 1; i <= 11; i++) {
                 if (p1.contains(count)) {
-                    System.out.print(BLUE + "B" + BLACK);
+                    System.out.print(BLUE + "B" + RESET);
                 }
                 else if (p2.contains(count)) {
-                    System.out.print(RED + "R"+ BLACK);
+                    System.out.print(RED + "R"+ RESET);
                 }
                 else System.out.print(0);
                 System.out.print(" ");
@@ -32,36 +30,64 @@ public class Hex {
         }
     }
 
+    private static void initializeEdges(UnionFind board) {
+        // 122 = TOP
+        // 123 = BOTTOM
+        // 124 = LEFT
+        // 125 = RIGHT
+        for (int i = 1; i < board.size; i++) {
+            if (i <= 11) board.Union(122, i);
+            if (i >= 111 && i <= 121) board.Union(123, i);
+            if ((i-1) % 11 == 0 && i > 1 && i < 121) board.Union(124, i);
+            if (i % 11 == 0 && i > 11 && i < 121) board.Union(125, i);
+        }
+    }
+
+
 
     public static void main(String[] args) throws FileNotFoundException {
-        String filename = "moves.txt";
+        String filename = "moves2.txt";
         Scanner fileScanner = new Scanner(new File("data/"+filename));
-        UnionFind  board = new UnionFind(121);
+        UnionFind board = new UnionFind(126);
+        initializeEdges(board);
+
         ArrayList<Integer> p1 = new ArrayList<>();
         ArrayList<Integer> p2 = new ArrayList<>();
         int turn = 1;
 
-        // read file and separate into p1 and 2
+        // read file and separate into p1 and p2
         while (fileScanner.hasNextInt()) {
-            if (turn == 1) {
+            if (turn == 1) {                        // player 1 (Blue)
                 p1.add(fileScanner.nextInt());
                 turn++;
             }
-            else {
+            else {                                  // player 2 (Red)
                 p2.add(fileScanner.nextInt());
                 turn--;
             }
         }
+
         for (int i = 0; i < p1.size(); i++) {
-            if (i+1 < p1.size()) {
+            if (i+1 < p1.size() && board.getNeighbors(p1.get(i)).contains(p1.get(i+1))) {
                 board.Union(p1.get(i), p1.get(i+1));
             }
         }
         for (int i = 0; i < p2.size(); i++) {
-            if (i+1 < p2.size()) {
+            if (i+1 < p2.size() && board.getNeighbors(p2.get(i)).contains(p2.get(i+1))) {
                 board.Union(p2.get(i), p2.get(i+1));
             }
         }
-        printBoard(board, p1, p2);
+
+//        for (int i = 122; i <= 125; i++) {
+//            System.out.println(board.find(i));
+//        }
+
+        int moves = p1.size() + p2.size();
+
+        if (board.find(122) == board.find(123)) System.out.println("Red Wins after " + moves + " moves");
+        if (board.find(124) == board.find(125)) System.out.print("Blue Wins after " + moves + " moves");
+
+        System.out.println();
+        printBoard(p1, p2);
     }
 }
--- src/UnionFind.java
+++ src/UnionFind.java
@@ -1,19 +1,17 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 // Union by Size with Path Compression
 public class UnionFind {
     int size;
-    public int[] id;
-    public int[] treeSize;
+    public int[] id; // root of the element
+    public int[] treeSize; // size of the set (compare this for Union by size)
 
     UnionFind(int size) {
         this.size = size;
-        id = new int[size];
-        treeSize = new int[size];
+        id = new int[size+1];
+        treeSize = new int[size+1];
 
         // Initialize the elements to be their own roots (trees of size 1)
         for (int i = 0; i < size; i++) {
@@ -45,6 +43,7 @@ public class UnionFind {
     // Find element and return root with path compression along the way
     public int find(int i) {
         int root = i;
+//        return id[i];
         while (root != id[root]) {
             root = id[root];
         }
@@ -57,8 +56,56 @@ public class UnionFind {
         return root;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
 
+
+    public ArrayList<Integer> getNeighbors(int num) {
+        ArrayList<Integer> neighbors = new ArrayList<>();
+//        x-1, x+1,x-11, x-10, x+10, x+11
+        if (num - 1 >= 1) {
+            neighbors.add(num-11);
+        }
+        if (num + 1 <= 121) {
+            neighbors.add(num+1);
+        }
+        if (num - 11 >= 1) {
+            neighbors.add(num-11);
+        }
+        if (num - 10 >= 1) {
+            neighbors.add(num-10);
+        }
+        if (num + 10 <= 121) {
+            neighbors.add(num+10);
+        }
+        if (num + 11 <= 121) {
+            neighbors.add(num+11);
+        }
+        return neighbors;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        int NumElements = 128;
+        int NumInSameSet = 16;
+
+        UnionFind ds = new UnionFind( NumElements );
+        int set1, set2;
+
+        for( int k = 1; k < NumInSameSet; k *= 2 )
+        {
+            for( int j = 0; j + k < NumElements; j += 2 * k )
+            {
+                set1 = ds.find( j );
+                set2 = ds.find( j + k );
+                ds.Union( set1, set2 );
+            }
+        }
+
+        for( int i = 0; i < NumElements; i++ )
+        {
+            System.out.print( ds.find( i )+ "*" );
+            if( i % NumInSameSet == NumInSameSet - 1 )
+                System.out.println( );
+        }
+        System.out.println( );
 
     }
 }
